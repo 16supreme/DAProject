2a0975a29bb0cc29408580519f6c3cd89a6533fe
tomasmonteiro <2231659@my.ipleiria.pt>
2024-05-15 09:51:38 +0100

    Adicionar arquivos de projeto.

---
+++ DAProject/Models/CantinaContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DAProject.Classes;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace DAProject
+{
+    public class CantinaContext : DbContext
+    {
+        public DbSet<Utilizador> Utilizadores { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Estudante> Estudantes { get; set; }
+        public DbSet<Professor> Professores { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+        public DbSet<Multa> Multas { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Prato> Pratos { get; set; }
+        public DbSet<Extra> Extras { get; set; }
+        public DbSet<Fatura> Faturas { get; set; }
+        public DbSet<ItemFatura> ItensFatura { get; set; }
+
+    }
+}
---
+++ DAProject/Models/Classes.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAProject
+{
+    public class Classes
+    {
+        public class Utilizador
+        {
+            public int id { get; set; }
+            public string nome { get; set; }
+            public int nif { get; set; }
+        }
+        public class Funcionario      : Utilizador
+        {
+            public string username;
+        }
+        public class Cliente : Utilizador
+        {
+            public float saldo;
+        }
+        public class Estudante : Cliente
+        {
+            public int numEstudante;
+        }
+        public class Professor : Cliente
+        {
+            public string email;
+        }
+
+        public class Reserva
+        {
+            public int id { get; set; }
+            public Cliente cliente;
+            public Menu menu;
+            public Prato prato;
+            public List<Extra> extras;
+            public Multa multa;
+        }
+        public class Multa
+        {
+            public int id { get; set; } 
+            float valor;
+            int numHoras;
+        }
+        public class Menu
+        {
+            public int id { get; set; } 
+            DateTime dataHora;
+            int qtdDisponivel;
+            float precoEstudante;
+            float precoProfessor;
+            List<Prato> pratos;
+            List<Extra> extras;
+        }
+        public class Prato
+        {
+            public int id { get; set; }
+            string descricao;
+            string tipo;
+            bool ativo;
+            List<Menu> menus;
+        }
+        public class Extra
+        {
+            public int id { get; set; }
+            string descricao;
+            float preco;
+            bool ativo;
+            List<Menu> menus;
+        }
+        public class Fatura
+        {
+            public int id { get; set; }
+            float total;
+            DateTime dataHora;
+            Cliente cliente;
+            Menu menu;
+        }
+        public class ItemFatura
+        {
+            public int id { get; set; }
+            string descricao;
+            float preco;
+            Fatura fatura;
+        }
+    }
+}
